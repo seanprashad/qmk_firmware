dfa125663a0ddf22f1dd5260d488fccc9d02cbfc
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2022-05-22 13:27:07 -0400

    Set DEBOUNCE time to 16ms

--- keyboards/gmmk/pro/config.h
+++ keyboards/gmmk/pro/config.h
@@ -109,8 +109,8 @@
 /* Send up to 4 key press events per scan */
 #define QMK_KEYS_PER_SCAN 4
 
-/* Set debounce time to 8ms */
-#define DEBOUNCE 8
+/* Set debounce time to 16ms */
+#define DEBOUNCE 16
 
 /* Force NKRO on boot up regardless of the setting saved in the EEPROM (uncomment to enable it) */
 // #define FORCE_NKRO
