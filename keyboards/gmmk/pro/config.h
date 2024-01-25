96efdbf468b098e3f9a502b4c7707af30b88ae50
Sean Prashad <13009507+SeanPrashad@users.noreply.github.com>
2024-01-25 14:30:08 -0500

    Set DEBOUNCE to 8ms Please enter the commit message for your changes. Lines starting

--- keyboards/gmmk/pro/config.h
+++ keyboards/gmmk/pro/config.h
@@ -114,7 +114,7 @@
 #define QMK_KEYS_PER_SCAN 4
 
 /* Set debounce time to 5ms */
-#define DEBOUNCE 5
+#define DEBOUNCE 8
 
 /* Force NKRO on boot up regardless of the setting saved in the EEPROM (uncomment to enable it) */
 // #define FORCE_NKRO
